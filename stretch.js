bd428efd002412d5f537723062ae6f2da5f86377
David Gold <davidgold@hey.com>
2020-07-20 19:35:47 -0400

    updated stretch

--- stretch.js
+++ stretch.js
@@ -10,7 +10,7 @@
 
 // I predict the output will be:
 'a defined? false' - b is most recently defined so it's read as a; or a = undefined
-'b defined? true'
+'b defined? true' - 
     
 //     2. Write a function that would allow you to do this using a closure. (This is another interview question we've seen before - when you're ready for answers, view an explanation [here](https://www.coderbyte.com/algorithm/3-common-javascript-closure-questions)).
     
@@ -19,7 +19,11 @@
 //     addSix(10); // returns 16
 //     addSix(21); // returns 27
 //     ```
-    
+    const addSix = function(num) {
+        return function createBase {
+            return num + 6;
+        }
+    }
 //     3. Research the differences between functional programming and object oriented programming. Then, describe the pros and cons of functional programming vs object-oriented programming. This is a common interview question and great practice!
     
 //     ## Resources
